3be07a2ec3c5e5831aedb030f42777636de57c9b
AldiraPutri19 <115212821+AldiraPutri19@users.noreply.github.com>
2023-10-03 09:51:14 +0800

    Add files via upload

---
+++ Praktikum0/PRAK001_2210817220024_AldiraPutriSholeha.java
@@ -0,0 +1,9 @@
+package Praktikum1;
+
+public class PRAK001_2210817220024_AldiraPutriSholeha {
+    public static void main (String[] args) {
+        System.out.println("Selamat Pagi, Aldira Putri Sholeha");
+        System.out.println("Selamat Siang, Aldira Putri Sholeha");
+        System.out.println("Selamat Malam, Aldira Putri Sholeha");
+    }
+}
